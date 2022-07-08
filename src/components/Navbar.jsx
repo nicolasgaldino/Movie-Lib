9e0e8c1be39904cccd0be74f52672d150499988f
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-08 11:26:45 -0300

    Página de busca totalmente funcional.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
-import { Link } from 'react-router-dom'
-import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
 import styles from './Navbar.module.css';
 
+
 const Navbar = () => {
+
+  const [search, setSearch] = React.useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch('');
+
+  };
+
   return (
     <>
       <nav className={styles.navbar}>
         <h2>
           <Link to='/'>MoviesLib <BiCameraMovie /></Link>
         </h2>
-        <form>
-          <input type='text' placeholder='Busque um filme' />
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={search}
+            placeholder='Busque um filme'
+            onChange={(event) => setSearch(event.target.value)}
+          />
           <button type='submit'>
             <BiSearchAlt2 />
           </button>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,39 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+import styles from './MovieGrid.module.css';
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Search = () => {
+
+  const [searchParams] = useSearchParams();
+  const [movies, setMovies] = React.useState([]);
+  const query = searchParams.get('q')
+
+  const getSearchMovies = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setMovies(data.results);
+  }
+
+  React.useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+    getSearchMovies(searchWithQueryURL);
+  }, [query])
+
   return (
-    <div>Search</div>
+    <div className={styles.container}>
+      <h2 className={styles.title}>
+        Resultados para: <span className={styles.queryText}>{query}</span>
+      </h2>
+      <div className={styles.moviesContainer}>
+        {movies.length === 0 && <p>Carregando...</p>}
+        {movies.length > 0 && movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+      </div>
+    </div>
   )
 }
 
