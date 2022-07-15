0f24aa5a0f76fdd02d09e20838f4d073c2ab9857
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-15 16:58:44 -0300

    Componente de Loading redesenhado.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,13 @@ import styles from './Loader.module.css';
 const Loader = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.containerLoader}></div>
+      <div className={styles.spinner}>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+      </div>
     </div>
   )
 }
